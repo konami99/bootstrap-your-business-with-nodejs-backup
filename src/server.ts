2ddf03244115ba468f81d5f62f3e25f55589e380
Richard Chou <richard.chou@shippit.com>
2021-01-12 22:09:53 +1100

    node cluster

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,46 @@
 import express, { Request, Response, NextFunction } from 'express';
+import os from 'os';
+import cluster from 'cluster';
 import { json } from 'body-parser';
-
 import todoRoutes from './routes/todos';
 
-const app = express();
+const clusterWorkerSize = os.cpus().length;
 
-app.use(json());
+if (clusterWorkerSize > 1) {
+  if (cluster.isMaster) {
+    console.log('spawning master');
+    for (let i=0; i < clusterWorkerSize; i++) {
+      cluster.fork()
+    }
 
-app.use('/todos', todoRoutes);
+    cluster.on("exit", function(worker) {
+      console.log("Worker", worker.id, " has exitted.")
+    })
+  } else {
+    console.log('spawning child process');
+    const app = express();
 
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-  res.status(500).json({ message: err.message });
-});
+    app.use(json());
 
-app.listen(3000);
+    app.use('/todos', todoRoutes);
+
+    app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+      res.status(500).json({ message: err.message });
+    });
+
+    app.listen(3000);
+  }
+} else {
+  const app = express();
+
+  app.use(json());
+
+  app.use('/todos', todoRoutes);
+
+  app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    res.status(500).json({ message: err.message });
+  });
+
+  app.listen(3000);
+}
+
