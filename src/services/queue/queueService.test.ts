dc06cb9afba7be00ce0cca47e984b232e66414b1
Richard Chou <richard.chou@shippit.com>
2021-01-16 07:48:56 +1100

    improves specs on QueueService

--- src/services/queue/queueService.test.ts
+++ src/services/queue/queueService.test.ts
@@ -1,12 +1,13 @@
 import QueueService from './queueService'
 import { Queue } from 'node-resque';
 
-describe('', () => {
-  it('', async () => {
-    const enqueueSpy = jest.spyOn(Queue.prototype, 'enqueue')
+describe('QueueService', () => {
+  it('calls enqueue on Queue', async () => {
+    const enqueueMock = jest.fn();
+    Queue.prototype.enqueue = enqueueMock;
 
     await QueueService.enqueue('queue1', 'sendEmail');
 
-    expect(enqueueSpy).toBeCalled()
+    expect(enqueueMock).toBeCalled()
   })
 })
