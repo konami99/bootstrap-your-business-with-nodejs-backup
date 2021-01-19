1dbea7303d41ad4c391bcd358c21f827077f74c9
Richard Chou <richard.chou@shippit.com>
2021-01-19 23:36:02 +1100

    mock Queue constructor

--- src/services/queue/queueService.test.ts
+++ src/services/queue/queueService.test.ts
@@ -1,11 +1,27 @@
 import QueueService from './queueService'
 import { Queue } from 'node-resque';
 
+// see https://jestjs.io/docs/en/es6-class-mocks#mocking-non-default-class-exports
+const enqueueMock = jest.fn();
+
+jest.mock('node-resque', () => {
+  return { 
+    Queue: jest.fn().mockImplementation(() => {
+      return {
+        connect: jest.fn(),
+        enqueue: enqueueMock,
+        end: jest.fn()
+      }
+    }),
+  }
+});
+
+afterEach(() => {
+  jest.clearAllMocks();
+});
+
 describe('QueueService', () => {
   it('calls enqueue on Queue', async () => {
-    const enqueueMock = jest.fn();
-    Queue.prototype.enqueue = enqueueMock;
-
     await QueueService.enqueue('queue1', 'sendEmail');
 
     expect(enqueueMock).toBeCalled()
