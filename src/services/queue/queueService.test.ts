3b7694c3d54ef6dc7bedee1a8df9e9f02e9d483c
Richard Chou <richard.chou@shippit.com>
2021-01-30 16:44:55 +1100

    fix specs

--- src/services/queue/queueService.test.ts
+++ src/services/queue/queueService.test.ts
@@ -24,6 +24,6 @@ describe('QueueService', () => {
   it('calls enqueue on Queue', async () => {
     await QueueService.enqueue('queue1', 'sendEmail');
 
-    expect(enqueueMock).not.toBeCalled()
+    expect(enqueueMock).toBeCalled()
   })
 })
